ade290e4a24bc574a0851398c24e1df63c1a8808
Md Ridowan Chowdhury <ridowan.chowdhury@bjitgroup.com>
2021-10-07 01:43:18 +0600

    mutationobserver added

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -22,31 +22,102 @@ class Test extends Component{
         this.calculate = this.calculate.bind(this)
         this.parsing=this.parsing.bind(this)
     }
-    componentDidMount(){
-        this.getButton()
-    }
-    getButton(){    
-        var nodeList = TestService.getTreeWalkerNodeList()
-        TestService.getTranslatedValue(nodeList,this.state.target,this.state.source)
-          .then(response=>{
-            TestService.getTreeWalkerContent(response)
-          })
 
-          if(this.state.target==='JA'){
-              console.log("Target JA found")
-            this.setState({
-                target:'EN',
-                source:'JA'
+    getButton() {
+        var nodeList = TestService.getTreeWalkerNodeList()
+        TestService.getTranslatedValue(nodeList, this.state.target, this.state.source)
+            .then(response => {
+                TestService.getTreeWalkerContent(response)
+                this.Observer()
             })
-        }else{
+
+        if (this.state.target === 'JA') {
+            console.log("Target JA found")
+            this.setState({
+                target: 'EN',
+                source: 'JA'
+            })
+        } else {
             console.log("Target EA found")
             this.setState({
-                target:'JA',
-                source:'EN'
+                target: 'JA',
+                source: 'EN'
             })
         }
-        console.log("target: "+this.state.target)
-        console.log("source: "+this.state.source)  
+        console.log("target: " + this.state.target)
+        console.log("source: " + this.state.source)
+
+    }
+
+    Observer() {
+        const mutationObserver = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                console.log(mutation)
+                if(mutation.addedNodes.length>0) {
+                    if(mutation.target.textContent.length>0) {
+                        console.log(mutation.target.textContent)
+                        mutationObserver.disconnect()
+                        var treeWalker = document.createTreeWalker(
+                            mutation.addedNodes[0],
+                            NodeFilter.SHOW_TEXT,
+                            { acceptNode: function(node) { return NodeFilter.FILTER_ACCEPT; } },
+                            false
+                        );
+                        var nodeList = [];
+                        var currentNode = treeWalker.currentNode;
+                        console.log("Testing....")
+                        var i = 0;
+                        while(treeWalker.nextNode()) {
+                            if(treeWalker.currentNode.textContent.trim().length<1) {
+                                continue;
+                            }
+                            // treeWalker.currentNode.textContent = 'badsha'
+                            nodeList.push(treeWalker.currentNode.textContent)
+                            i++
+                        }
+
+                        TestService.getTranslatedValue(nodeList, 'JA', 'EN')
+                            .then(response => {
+                                var treeWalker2 = document.createTreeWalker(
+                                    mutation.addedNodes[0],
+                                    NodeFilter.SHOW_TEXT,
+                                    { acceptNode: function(node) { return NodeFilter.FILTER_ACCEPT; } },
+                                    false
+                                );
+                                var nodeList2 = [];
+                                var currentNode2 = treeWalker.currentNode;
+                                console.log("Testing2....")
+                                var i = 0;
+                                while(treeWalker2.nextNode()) {
+                                    if(treeWalker2.currentNode.textContent.trim().length<1) {
+                                        continue;
+                                    }
+                                    treeWalker2.currentNode.textContent= response.data[i]
+                                    i++
+                                }
+                            })
+
+                        mutationObserver.observe(document.documentElement, {
+                            attributes: true,
+                            characterData: true,
+                            childList: true,
+                            subtree: true,
+                            attributeOldValue: true,
+                            characterDataOldValue: true
+                        });
+
+                    }
+                }
+            });
+        });
+        mutationObserver.observe(document.documentElement, {
+            attributes: true,
+            characterData: true,
+            childList: true,
+            subtree: true,
+            attributeOldValue: true,
+            characterDataOldValue: true
+        });
     }
     calculate(){
         let a=3*5
